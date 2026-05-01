748fff5f4216a6b64bc64ece0eb042627a267742
Greg Granger <flockos.talk@gmail.com>
2026-04-30 18:47:46 -0700

    feat: add 'continue learning in GROW' link alongside Request Access on login screen

--- New_Covenant/Scripts/the_priesthood/the_garments.js
+++ New_Covenant/Scripts/the_priesthood/the_garments.js
@@ -47,6 +47,8 @@ export async function renderGarments(/* { mode } */) {
 
         <p class="garments-request-access">
           Not set up yet? <button type="button" class="garments-request-link" data-act="request-access">Request access</button>
+          <span class="garments-request-sep">·</span>
+          or <a href="grow-public.html" class="garments-request-link">continue learning in GROW</a>
         </p>
       </form>
     `;
